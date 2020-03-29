677d2d6f5f51de13769675ce233ed611599778b2
grafcheg <dergraff@gmail.com>
2020-03-29 14:19:11 +1000

    rework

--- app.js
+++ app.js
@@ -12,34 +12,40 @@ let connections = [];
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html; charset=utf-8");
   res.setHeader("Transfer-Encoding", "chunked");
-  connections.push(res);
+  connections.push([res, LIMIT]);
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-let tick = 0;
-setTimeout(function run() {
-  const today = new Date();
-  const year = today.getUTCFullYear();
-  const month = today.getUTCMonth();
-  const day = today.getUTCDate();
-  const hours = today.getUTCHours();
-  const minutes = today.getUTCMinutes();
-  const seconds = today.getUTCSeconds();
+const tickFn = () => {
+  setTimeout(() => {
+    const today = new Date();
+    const year = today.getUTCFullYear();
+    const month = today.getUTCMonth();
+    const day = today.getUTCDate();
+    const hours = today.getUTCHours();
+    const minutes = today.getUTCMinutes();
+    const seconds = today.getUTCSeconds();
 
-  console.log(`Time: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}`);
-  if (++tick > LIMIT) {
-    connections.map(res => {
-      res.write(`Goodbye time: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}\n`);
-      res.end();
+    // console.log(`Time: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}`);
+
+    connections = connections.filter(connection => {
+      if (connection[1]-- > 0) {
+        connection[0].write(`Time: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}\n`);
+
+        return true;
+      } else {
+        connection[0].write(`Goodbye time: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}\n`);
+        connection[0].end();
+
+        return false;
+      }
     });
-    connections = [];
-    tick = 0;
-  }
-  connections.map((res, i) => {
-    res.write(`USER#${i}! Time: ${day}.${month}.${year} ${hours}:${minutes}:${seconds}.\n`);
-  });
-  setTimeout(run, DELAY);
-}, DELAY);
+
+    tickFn();
+  }, DELAY);
+};
+
+tickFn();
